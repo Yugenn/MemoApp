2898a25e0d0b4022468e9ebb4244a15826d0f837
Yugenn <yuruseajyan@gmail.com>
2021-10-31 11:50:16 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 祐源宏行 on 2021/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // textViewがUITextViewDelegateを使えるようにするおまじない。
+        textView.delegate = self
+        
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        sceneDelegate.lastText = textView.text
+    }
+
+
+}
+
